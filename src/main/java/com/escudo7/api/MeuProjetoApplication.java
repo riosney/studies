3c0116224e79a955b71548ab227d6bd4501509b2
Riosney Santos <riosney_s@hotmail.com>
2021-08-14 21:24:53 -0300

    criação do projeto

---
+++ src/main/java/com/escudo7/api/MeuProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.escudo7.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoApplication.class, args);
+	}
+
+}
