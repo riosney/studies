bafa22ff099b6559e0c564270fd83585f2728ccc
Riosney <rsantosdo@minsait.com>
2022-05-09 10:07:37 -0300

    Class 02 Vetor

---
+++ src/com/riosney/estruturadados/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.riosney.estruturadados.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+	
+}
---
+++ src/com/riosney/estruturadados/vetor/teste/Class02.java
@@ -0,0 +1,16 @@
+package com.riosney.estruturadados.vetor.teste;
+
+import com.riosney.estruturadados.vetor.Vetor;
+
+public class Class02 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(5);
+		
+		int i=0;
+		i++;
+		
+	}
+
+}
