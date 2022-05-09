c7fc6eb1f47f9483586693a858646989961bf7f1
Riosney <rsantosdo@minsait.com>
2022-05-09 11:07:42 -0300

    Vetores e Arrays, tamanhos e imprimir elementos

---
+++ src/com/riosney/estruturadados/vetor/teste/Class04.java
@@ -0,0 +1,21 @@
+package com.riosney.estruturadados.vetor.teste;
+
+import com.riosney.estruturadados.vetor.Vetor;
+
+public class Class04 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(10);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+		System.out.println(vetor.tamanho());
+		
+		System.out.println(vetor.toString());
+		
+	}
+
+}
