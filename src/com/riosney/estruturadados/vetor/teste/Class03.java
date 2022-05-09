26906cb20794d9e4341087a3e7beed8445ecf4fe
Riosney <rsantosdo@minsait.com>
2022-05-09 10:35:14 -0300

    class 03 vetor

---
+++ src/com/riosney/estruturadados/vetor/teste/Class03.java
@@ -0,0 +1,25 @@
+package com.riosney.estruturadados.vetor.teste;
+
+import com.riosney.estruturadados.vetor.Vetor;
+
+public class Class03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+		
+		/*try {
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("elemento 2");
+			vetor.adiciona("elemento 3");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+	}
+
+}
