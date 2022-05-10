8ff3e0a32aab2bf504e21e487769f69fa3731b74
Riosney <rsantosdo@minsait.com>
2022-05-10 09:34:17 -0300

    Vetores. Verifica se elemento existe

---
+++ src/com/riosney/estruturadados/vetor/teste/Class06.java
@@ -0,0 +1,20 @@
+package com.riosney.estruturadados.vetor.teste;
+
+import com.riosney.estruturadados.vetor.Vetor;
+
+public class Class06 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(10);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+		System.out.println(vetor.busca("elemento 1"));
+		System.out.println(vetor.busca("elemento 4"));
+		
+	}
+
+}
