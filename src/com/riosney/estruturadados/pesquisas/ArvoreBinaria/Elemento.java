5c7a3caec2f4e7d0f1789c78199ee2e52b9bcdbe
Riosney <rsantosdo@minsait.com>
2022-07-18 15:00:42 -0300

    Árvore Binária de busca

---
+++ src/com/riosney/estruturadados/pesquisas/ArvoreBinaria/Elemento.java
@@ -0,0 +1,21 @@
+package com.riosney.estruturadados.pesquisas.ArvoreBinaria;
+
+public class Elemento {
+	
+	private int valor;
+	
+	public Elemento(int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+
+}
